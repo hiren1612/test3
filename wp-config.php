2dc002f8e8b6f30ce7d72decfcd06d6d5393a292
hiren1612 <patelhiren16@gmail.com>
2021-07-08 10:43:14 +0530

    commit

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define( 'DB_USER', 'u813082887_Test' );
 define( 'DB_PASSWORD', 'Test@1234' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
